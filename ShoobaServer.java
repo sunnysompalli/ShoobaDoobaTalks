f2ce7535ded2ad0e2e405bc96527c1f2e6ac242a
Sunny Sompalli <sunnysompalli@gmail.com>
2016-05-17 12:20:10 -0400

    Did a little

--- ShoobaServer.java
+++ ShoobaServer.java
@@ -1,6 +1,14 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
+
 public class ShoobaServer{
+
+    private static final int port = 0;
+
+    public static void main(String[]args){
+	ServerSocket s = new ServerSocket(port);
+    }
+    
 
 }
